ad2b08ae73cf75e5ec0a16e818798d0451c0ff02
FarmGTEx <109404196+FarmGTEx@users.noreply.github.com>
2026-03-11 17:03:26 +0100

    Update prepare_QTLenrich.sh

--- 07.GWAS_and_molQTL/01.QTLenrich/prepare_QTLenrich.sh
+++ 07.GWAS_and_molQTL/01.QTLenrich/prepare_QTLenrich.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-#--------------------------------------------------------------------------#
-#              Edit Job specifications                                     #
-#--------------------------------------------------------------------------#
-#SBATCH -p normal                 # Name of the queue
-#SBATCH -N 1                       # Number of nodes(DO NOT CHANGE)
-#SBATCH -n 12                       # Number of CPU cores
-#SBATCH --mem=512000               # Memory in MiB(10 GiB = 10 * 1024 MiB)
-#SBATCH -J ieQTL                # Name of the job
-#SBATCH --output=slurm_%A.out   # STDOUT
-#SBATCH --error=slurm_%A.err    # STDERR
-#SBATCH -t 10:00:00              # Job max time - Format = MM or MM:SS or HH:MM:SS or DD-HH or DD-HH:MM
-# Create a temporary directory for the job in local storage - DO NOT CHANGE #
-TMPDIR=/scratch/$USER/$SLURM_JOBID
-export TMPDIR
-mkdir -p $TMPDIR
-#=========================================================================#
-#         Your job script                                                 #
-#=========================================================================#
 
 cd /faststorage/project/cattle_gtexs/script/QTLenrich/
 
@@ -27,9 +9,3 @@ python3 create_null_variants.py --all_variants_file /faststorage/project/cattle_
 python3 Generate_Null_Table.py --QTL_Directory /faststorage/project/farmgtex/Figure6/QTLenrich/eQTL/all_pairs/ --File_Extension .eqtl_allpairs.txt --Null_Variants_List /faststorage/project/farmgtex/Figure6/QTLenrich/3aQTL/Group2_null_variants.txt --Output_File /faststorage/project/farmgtex/Figure6/QTLenrich/3aQTL/Group2_null_variants_table.txt.gz
 
 python3 Generate_Confounders_Table.py --QTL_Directory /faststorage/project/farmgtex/Figure6/QTLenrich/eQTL/all_pairs/ --File_Extension .eqtl_allpairs.txt --Variants_List /faststorage/project/cattle_gtexs/CattleGTEx/OmiGA/QTLenrich/ct_seQTL/all_variant.txt --LD_Proxy /faststorage/project/cattle_gtexs/CattleGTEx/OmiGA/QTLenrich/ct_seQTL/number_ld_per_variant.txt --GENCODE_File /faststorage/project/cattle_gtexs/CattleGTEx/OmiGA/QTLenrich/ct_seQTL/Bos_taurus.ARS-UCD1.2.gtf --Output_File /faststorage/project/farmgtex/Figure6/QTLenrich/3aQTL/Group2_output_confounder.txt.gz
-
-#=========================================================================#
-#         Cleanup  DO NOT REMOVE OR CHANGE                                #
-#=========================================================================#
-cd $SLURM_SUBMIT_DIR
-rm -rf /scratch/$USER/$SLURM_JOBID
