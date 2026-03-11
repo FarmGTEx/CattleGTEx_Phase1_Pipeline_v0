886108db06e51f07ca894a9bdfaef770b8e6e943
FarmGTEx <109404196+FarmGTEx@users.noreply.github.com>
2026-03-11 17:03:42 +0100

    Update run_QTLEnrichV2.sh

--- 07.GWAS_and_molQTL/01.QTLenrich/run_QTLEnrichV2.sh
+++ 07.GWAS_and_molQTL/01.QTLenrich/run_QTLEnrichV2.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-#--------------------------------------------------------------------------#
-#              Edit Job specifications                                     #
-#--------------------------------------------------------------------------#
-#SBATCH -p normal                 # Name of the queue
-#SBATCH -N 1                       # Number of nodes(DO NOT CHANGE)
-#SBATCH -n 24                       # Number of CPU cores
-#SBATCH --mem=102400                # Memory in MiB(10 GiB = 10 * 1024 MiB)
-#SBATCH -J ieQTL                # Name of the job
-#SBATCH --output=slurm_%A.out   # STDOUT
-#SBATCH --error=slurm_%A.err    # STDERR
-#SBATCH -t 10:00:00              # Job max time - Format = MM or MM:SS or HH:MM:SS or DD-HH or DD-HH:MM
-# Create a temporary directory for the job in local storage - DO NOT CHANGE #
-TMPDIR=/scratch/$USER/$SLURM_JOBID
-export TMPDIR
-mkdir -p $TMPDIR
-#=========================================================================#
-#         Your job script                                                 #
-#=========================================================================#
 
 cd /faststorage/project/cattle_gtexs/script/QTLenrich/QTLEnrich-master/src
 main_dir="/faststorage/project/cattle_gtexs/CattleGTEx/OmiGA/QTLenrich/state_ieQTL/result/"
@@ -35,8 +17,3 @@ for trait_dir in "$main_dir"/*/; do
                              --output_directory /faststorage/project/farmgtex/Figure6/QTLenrich/3aQTL/tissue_shared/result/Group6/${trait_name}/ \
                              --GeneEnrich_input
 done
-#=========================================================================#
-#         Cleanup  DO NOT REMOVE OR CHANGE                                #
-#=========================================================================#
-cd $SLURM_SUBMIT_DIR
-rm -rf /scratch/$USER/$SLURM_JOBID
